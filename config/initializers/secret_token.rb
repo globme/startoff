94e8f642f88d88b49795a40fa981409031259e63
Thomas Thiel <thiel.thomas@gmail.com>
2013-11-21 23:18:46 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Startoff::Application.config.secret_token = 'caac2ab2ee2f5fb61b5b8c0d9d9213d15c641481a2204698fb43321e21a9361fd296fe778c053c9f54dc89e272aa80b1c4aa718635423e2464635230ddea4711'
